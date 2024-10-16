131725764184ceafb87f90ff46b1b0a0edf6550f
mad <mad.git.off@gmail.com>
2024-10-16 14:46:45 -0500

    Created statistics

--- src/app/i/page.tsx
+++ src/app/i/page.tsx
@@ -1,6 +1,10 @@
 import { Metadata } from 'next'
 
+import { Heading } from '@/components/ui/Heading'
+
 import { NO_INDEX_PAGE } from '@/constants/seo.constants'
+
+import { Statistics } from './Statistics'
 
 export const metadata: Metadata = {
 	title: 'Dashboard',
@@ -8,5 +12,10 @@ export const metadata: Metadata = {
 }
 
 export default function DashboardPage() {
-	return <div>Dashboard</div>
+	return (
+		<div>
+			<Heading title='Statistics' />
+			<Statistics />
+		</div>
+	)
 }
