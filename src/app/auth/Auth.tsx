a4887b0b521e8c9ae78caec38f486cb81a4777bc
mad <mad.git.off@gmail.com>
2024-10-16 11:49:56 -0500

    Created register/login form

--- src/app/auth/Auth.tsx
+++ src/app/auth/Auth.tsx
@@ -6,6 +6,10 @@ import { useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 
+import { Heading } from '@/components/ui/Heading'
+import { Button } from '@/components/ui/buttons/Button'
+import { Field } from '@/components/ui/fields/Field'
+
 import { IAuthForm } from '@/types/auth.types'
 
 import { DASHBOARD_PAGES } from '@/config/pages-url.config'
@@ -42,7 +46,31 @@ export function Auth() {
 				className='w-1/4 m-auto shadow bg-sidebar rounded-xl p-layout'
 				onSubmit={handleSubmit(onSubmit)}
 			>
-				<div className='flex items-center gap-5 justify-center'></div>
+				<Heading title='Auth' />
+				<Field
+					{...register('email', {
+						required: 'Email is required!'
+					})}
+					id='email'
+					label='Email:'
+					placeholder='Enter email:'
+					type='email'
+					extra='mb-4'
+				/>
+				<Field
+					id='password'
+					label='Password:'
+					placeholder='Enter password:'
+					type='password'
+					{...register('password', {
+						required: 'Password is required!'
+					})}
+					extra='mb-6'
+				/>
+				<div className='flex items-center gap-5 justify-center'>
+					<Button onClick={() => setIsLoginForm(true)}>Login</Button>
+					<Button onClick={() => setIsLoginForm(false)}>Register</Button>
+				</div>
 			</form>
 		</div>
 	)
