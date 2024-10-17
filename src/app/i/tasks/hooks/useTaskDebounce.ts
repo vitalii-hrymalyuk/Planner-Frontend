3201a7634342469a5b637ab06b4980e95f1641a4
mad <mad.git.off@gmail.com>
2024-10-17 14:06:39 -0500

    Created hooks

---
+++ src/app/i/tasks/hooks/useTaskDebounce.ts
@@ -0,0 +1,48 @@
+import debounce from 'lodash.debounce'
+import { useCallback, useEffect } from 'react'
+
+import { TypeTaskFormState } from '@/types/task.types'
+import { UseFormWatch } from 'react-hook-form'
+import { useCreateTask } from './useCreateTask'
+import { useUpdateTask } from './useUpdateTask'
+
+interface IUseTaskDebounce {
+	watch: UseFormWatch<TypeTaskFormState>
+	itemId: string
+}
+
+export function useTaskDebounce({ watch, itemId }: IUseTaskDebounce) {
+
+	const { updateTask } = useUpdateTask()
+	const { createTask } = useCreateTask()
+
+	const debounceCreateTask = useCallback(
+		debounce((formData: TypeTaskFormState) => {
+			createTask(formData)
+		}, 444),
+		[]
+	)
+
+	const debounceUpdateTask = useCallback(
+		debounce((formData: TypeTaskFormState) => {
+			updateTask({ id: itemId, data: formData })
+		}, 444),
+		[]
+	)
+
+	useEffect(() => {
+		const { unsubscribe } = watch(formData => {
+			if (itemId) {
+				debounceUpdateTask({
+					...formData,
+					priority: formData.priority || undefined
+				})
+			} else {
+				debounceCreateTask(formData)
+			}
+		})
+		return () => {
+			unsubscribe()
+		}
+	}, [watch(), debounceUpdateTask, debounceCreateTask])
+}
